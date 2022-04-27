619c5faa3051c5a53540e255143509e6d578a100
maxdacho <92314290+maxdacho@users.noreply.github.com>
2022-04-27 14:09:14 +0200

    Stylen
    Erste Schritte beim Stylen

---
+++ animation.js
@@ -0,0 +1,1 @@
+document.getElementById("app").classList.add('is-loaded');
--- src/generateRandomNumber.ts
+++ src/generateRandomNumber.ts
@@ -2,7 +2,7 @@ import { outputFieldRandomNumber } from "./domElements";
 
 //Funktion die Zufallszahl generiert
 function generateRandomNumber(){
-    let randomNumber = Math.round(Math.random() * 10);
+    let randomNumber : number = Math.round(Math.random() * 10);
     outputFieldRandomNumber.innerHTML = `${randomNumber}`;
 }
 

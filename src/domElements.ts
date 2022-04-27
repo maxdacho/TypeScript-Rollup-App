6204285f5b6873be9b6748c2bb744622ebe2c90c
maxdacho <92314290+maxdacho@users.noreply.github.com>
2022-04-27 10:46:43 +0200

    Grundfunktionalität
    Aufteilung des Projekts in mehrere Modules (.ts-Dateien) sowie Erstellung der Grundfunktionalität zur Generierung einer Zufallszahl

--- src/domElements.ts
+++ src/domElements.ts
@@ -2,5 +2,10 @@
 let outputFieldRandomNumber = document.getElementById('outputRandomNumber') as HTMLElement;
 let buttonGenerateRandomNumber = document.getElementById('generateRandomNumber') as HTMLButtonElement;
 
+//Elemente zur Erstellung einer Liste mit Namen nach User Eingabe
+let inputFieldGiveNames = document.getElementById('addName') as HTMLInputElement;
+let outputFieldNamesfromUserEntry = document.getElementById('outputNamesFromList') as HTMLElement;
+let buttonOutputFieldGiveNamestoList = document.getElementById('buttonPutNameToList') as HTMLButtonElement;
+
 
 export{outputFieldRandomNumber, buttonGenerateRandomNumber};

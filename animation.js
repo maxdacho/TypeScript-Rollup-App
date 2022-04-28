48a46e0d9395152640e107699e7136d2d0dfa945
maxdacho <92314290+maxdacho@users.noreply.github.com>
2022-04-28 09:35:09 +0200

    Styling
    Weitere Arbeiten am Styling

--- animation.js
+++ animation.js
@@ -1,2 +1,4 @@
-document.getElementById("app").classList.add('is-loaded');
-document.getElementById("headline").classList.add('is-loaded');
+document.getElementById("appArea").classList.add('isLoaded');
+document.getElementById("headline").classList.add('isLoaded');
+document.getElementById("appArea2").classList.add('isLoaded2');
+
